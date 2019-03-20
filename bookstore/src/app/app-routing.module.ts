bb0f218e3de33389d937a6a523fa2a6f97fa816a
Aru01 <tutkenova.a@gmail.com>
2019-03-20 20:34:41 +0600

    bookstore page done

--- bookstore/src/app/app-routing.module.ts
+++ bookstore/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookstoreComponent } from './bookstore/bookstore.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bookstore', component: BookstoreComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
