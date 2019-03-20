4bb1bde0bdb7db117aca4b62c9aabcd3bf5cd82d
Meru <meruert_26.01@mail.ru>
2019-03-20 20:48:47 +0600

    Routing and home page

--- bookstore/src/app/app-routing.module.ts
+++ bookstore/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookstoreComponent } from './bookstore/bookstore.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'bookstore', component: BookstoreComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent},
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
