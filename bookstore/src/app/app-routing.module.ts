fa167344137cb75b980f09ffe152fe7ef7b66090
Aimira <aimiraabenova1999@gmail.com>
2019-03-20 20:57:23 +0600

    Routing for about page

--- bookstore/src/app/app-routing.module.ts
+++ bookstore/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BookstoreComponent } from './bookstore/bookstore.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'bookstore', component: BookstoreComponent },
   { path: 'contact', component: ContactComponent},
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
