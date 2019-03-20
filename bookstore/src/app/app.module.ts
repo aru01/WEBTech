a11c0a39c7059da6c9caf532e16eeb62f0acbaa7
Meru <meruert_26.01@mail.ru>
2019-03-20 20:44:38 +0600

    Created home component

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookstoreComponent } from './bookstore/bookstore.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookstoreComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
