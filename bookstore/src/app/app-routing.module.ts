4d062af2ef971e466ed46f36402d58331a039609
Aru01 <tutkenova.a@gmail.com>
2019-03-20 20:39:01 +0600

    added page contact

--- bookstore/src/app/app-routing.module.ts
+++ bookstore/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookstoreComponent } from './bookstore/bookstore.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'bookstore', component: BookstoreComponent }];
+  { path: 'bookstore', component: BookstoreComponent },
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
