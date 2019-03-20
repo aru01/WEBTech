bb77ede484233a87b0c5d6d87872fbaaa2ef4ac5
Aru01 <tutkenova.a@gmail.com>
2019-03-20 20:29:27 +0600

    create bookstore

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookstoreComponent } from './bookstore/bookstore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookstoreComponent
   ],
   imports: [
     BrowserModule,
