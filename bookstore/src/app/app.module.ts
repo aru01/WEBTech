edb57b301fa617af32e42475eb6e79a28e2dfb94
Aru01 <tutkenova.a@gmail.com>
2019-03-20 20:36:54 +0600

    create contact component

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookstoreComponent } from './bookstore/bookstore.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookstoreComponent
+    BookstoreComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
